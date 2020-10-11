98c678d6e3c302eadcd6b8d3cde1d3faebc94006
possible-gratitude <abayomi.gratitudeindia@gmail.com>
2020-10-11 17:00:58 +0100

    admin route protected

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
+    public function __construct(){
+        $this->middleware('checkRole:admin');
+    }
     public function index(){
         return view('admin.dashboard');
     }
